4ee7086f59cac40da7a92c5490c24db2ee551c18
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-01-09 17:46:49 +0900

    add todos count state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,16 @@
-import { atom } from 'recoil'
+import { atom,selector } from 'recoil'
 
 const initialTodo:string[] = []
 
-export const todos = atom({
-  key: 'todos',
+export const todosState = atom({
+  key: 'todosState',
   default: initialTodo,
 })
+
+const todosCountState = selector({
+    key: 'todosCount', // unique ID (with respect to other atoms/selectors)
+    get: ({get}) => {
+      const todos = get(todosState);
+      return todos.length;
+    },
+  });
